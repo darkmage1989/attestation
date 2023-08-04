c39f0b735d737fc4ed2f2d39a101650148e06f7c
darkmage1989 <darkmage1989@gmail.com>
2023-08-05 01:31:51 +0300

    Исправление после деплоя

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter/> 
     <Provider store={store}>
       <App />
     </Provider>
-    <HashRouter/> 
   </React.StrictMode>
 );
 
